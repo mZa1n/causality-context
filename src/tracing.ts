98af20631627f665ad4bcc7d60555b6bc8fa8f6f
za1n <k9209107535@yandex.ru>
2026-07-06 16:38:54 +0300

    fix бага с неймингом

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,9 +1,10 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { resourceFromAttributes } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { PrismaInstrumentation } from '@prisma/instrumentation';
+import {NodeSDK} from '@opentelemetry/sdk-node';
+import {getNodeAutoInstrumentations} from '@opentelemetry/auto-instrumentations-node';
+import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-http';
+import {resourceFromAttributes} from '@opentelemetry/resources';
+import {ATTR_SERVICE_NAME} from '@opentelemetry/semantic-conventions';
+import {PrismaInstrumentation} from '@prisma/instrumentation';
+import {ExpressLayerType} from '@opentelemetry/instrumentation-express';
 
 const serviceName = process.env.OTEL_SERVICE_NAME ?? 'unknown-service';
 const resource = resourceFromAttributes({ [ATTR_SERVICE_NAME]: serviceName });
@@ -15,7 +16,12 @@ const sdk = new NodeSDK({
         getNodeAutoInstrumentations({
             '@opentelemetry/instrumentation-fs': { enabled: false },
             '@opentelemetry/instrumentation-dns': { enabled: false },
-            '@opentelemetry/instrumentation-express': { enabled: false },
+            '@opentelemetry/instrumentation-express': {
+                ignoreLayersType: [
+                    ExpressLayerType.MIDDLEWARE,
+                    ExpressLayerType.REQUEST_HANDLER,
+                ]
+            },
             '@opentelemetry/instrumentation-router': { enabled: false },
             '@opentelemetry/instrumentation-net': { enabled: false },
         }),
